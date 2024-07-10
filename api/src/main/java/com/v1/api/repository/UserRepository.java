0f3b590fc458246afb1bd24b4bb3c26a11b05748
Carlos Soares <carloshenriquepereirasoaress@gmail.com>
2024-07-10 14:25:40 -0300

    add class userRepository

---
+++ api/src/main/java/com/v1/api/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.v1.api.repository;
+
+import com.v1.api.entitie.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
