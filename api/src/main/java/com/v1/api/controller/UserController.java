c88c9b21e40acee0b61ab9a648b131976e8dc76b
Carlos Soares <carloshenriquepereirasoaress@gmail.com>
2024-07-10 14:46:41 -0300

    class UserController refactor

--- api/src/main/java/com/v1/api/controller/UserController.java
+++ api/src/main/java/com/v1/api/controller/UserController.java
@@ -1,17 +1,22 @@
 package com.v1.api.controller;
 
 import com.v1.api.entitie.User;
+import com.v1.api.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
 
 
-    public ResponseEntity<User> userFindById(){
-        return null;
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> FindById(@PathVariable Integer id){
+        userService.finById(id);
+        return ResponseEntity.ok().build();
     }
 }
