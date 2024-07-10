037de538128a42f2bf06e1f375763057c2f8cbea
Carlos Soares <carloshenriquepereirasoaress@gmail.com>
2024-07-10 14:22:25 -0300

    add class user

---
+++ api/src/main/java/com/v1/api/entitie/User.java
@@ -0,0 +1,26 @@
+package com.v1.api.entitie;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+}
