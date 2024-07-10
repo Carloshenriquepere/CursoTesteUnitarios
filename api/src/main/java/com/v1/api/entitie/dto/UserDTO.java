4d96444ab1e3366134f4663feb5d51283075448a
Carlos Soares <carloshenriquepereirasoaress@gmail.com>
2024-07-10 15:45:59 -0300

    class UserDTO

---
+++ api/src/main/java/com/v1/api/entitie/dto/UserDTO.java
@@ -0,0 +1,22 @@
+package com.v1.api.entitie.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+
+    private Integer id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+
+}
